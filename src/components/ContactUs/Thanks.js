675f1fc3f7d88f565b56f64bb68ecc50c830761e
EZRankings-devs <110896205+EZRankings-devs@users.noreply.github.com>
2022-08-09 18:28:50 +0530

    Update Thanks.js

--- src/components/ContactUs/Thanks.js
+++ src/components/ContactUs/Thanks.js
@@ -1,8 +1,36 @@
-import React from 'react';
+import React from "react";
+import image from "../../assets/background.webp";
+import Vid4 from "../../assets/Vid4.mp4";
+import Vid5 from "../../assets/Vid5.mp4";
+import classes from "./AboutUs.module.css";
+import { Button } from "@material-ui/core";
+import { Link } from "react-router-dom";
+import SEO from "../../containers/SEO/SEO";
 
+const AboutUs = () => {
+  return (
+    <>
+      <SEO
+        title="Thank You"
+        description="Thank You"
+        keywords="African Safaris, Gorilla Safaris"
+      />
+      <div className={classes.about_us_wrapper}>
+        <div
+          className={classes.dav__about_us_hero}
+          style={{
+            backgroundImage: `url(${image})`,
+            backgroundSize: "cover",
+            backgroundPosition: "center center",
+          }}
+        >
+          <div className={classes.dav__about_us_title}>
+            <h2>Thanks For Contacting Us. Our Repersentative Will Contact You Shortly.</h2>
+          </div>
+        </div>
+        </div>
+    </>
+  );
+};
 
-function App() {
-    return <h2>Thank you</h2>;
-  }
-
-export default App;
+export default AboutUs;
