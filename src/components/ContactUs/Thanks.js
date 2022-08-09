fe5e32f3e63e1e9c291d7b89ec687f786e3773c0
EZRankings-devs <110896205+EZRankings-devs@users.noreply.github.com>
2022-08-09 18:12:16 +0530

    Update Thanks.js

--- src/components/ContactUs/Thanks.js
+++ src/components/ContactUs/Thanks.js
@@ -1,1 +1,27 @@
-<p>Message Sent Successfully, Dav Safaris will contact you shortly.</p>
+ lines (257 sloc) 10 KB
+import classes from "./ContactUs.module.css";
+import React, { useEffect, useState } from "react";
+import Image from "../../assets/background.webp";
+import Operator from "../../assets/davLogo.png";
+import RoomIcon from "@material-ui/icons/Room";
+import PhoneIcon from "@material-ui/icons/Phone";
+import { Button, Paper, TextField } from "@material-ui/core";
+import ContactUsForm from "../Tours/SingleTour/Bookings/ContactUsForm";
+import { EmailOutlined } from "@material-ui/icons";
+import OfficeIcon from "@material-ui/icons/QueryBuilder";
+import SocialMedia from "./SocialMedia";
+import { Form } from "react-bootstrap";
+import { NewsLetters } from "../../store/Actions/UserActions";
+import { useDispatch, useSelector } from "react-redux";
+import { Alert } from "@material-ui/lab";
+import SEO from "../../containers/SEO/SEO";
+
+function app(){
+  return(
+  <>
+   <p>Message Sent Successfully, Dav Safaris will contact you shortly.</p> 
+    </>
+  )
+}
+export default app;
+ 
