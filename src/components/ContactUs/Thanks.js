52ec9d48d8743a040dc3305ecad2dcf40a3c4dbd
EZRankings-devs <110896205+EZRankings-devs@users.noreply.github.com>
2022-08-10 17:16:09 +0530

    Update Thanks.js

--- src/components/ContactUs/Thanks.js
+++ src/components/ContactUs/Thanks.js
@@ -12,7 +12,7 @@ const Thanks = () => {
 
       <div>
          <br/><br/><br/><br/><br/><br/>
-        <h2 style="text-align:center;">
+        <h2>
           Thanks For Contacting Us. Our Repersentative Will Contact You Shortly.
         </h2>
     <br/><br/>
