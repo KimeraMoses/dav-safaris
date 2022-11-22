7e7ae2ac0053fc86fc009e8b3dde9dd619dd4cb2
KimeraMoses <kimeramoses001@gmail.com>
2022-11-22 12:40:19 +0300

    new: changed baseurl

--- src/store/index.js
+++ src/store/index.js
@@ -32,7 +32,8 @@ const store = configureStore({
 });
 
 export default store;
-export const baseUrl = "https://dav-safari.herokuapp.com";
+// export const baseUrl = "https://dav-safari.herokuapp.com";
+export const baseUrl = "https://davsafaris.herokuapp.com";
 // export const TRACKING_ID = "G-LJDPXC1D4T";
 export const TRACKING_ID = "UA-221371729-1";
 
