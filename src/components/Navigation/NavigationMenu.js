b4b700f6b496d67aa30a0697f382a85a97fa99a5
ssemugenyiisaac2@gmail.com <ssemugenyiisaac2@gmail.com>
2023-06-16 15:26:20 +0300

    (update): Added a dropdown in the navbar

--- src/components/Navigation/NavigationMenu.js
+++ src/components/Navigation/NavigationMenu.js
@@ -1,13 +1,15 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+
 import classes from "./NavigationMenu.module.css";
+import styles from "./NavDropDown.module.css";
 
 const NavigationMenu = (props) => {
   const { menuOpen, setMenuOpen } = props;
 
-  const NavMenuItem = ({ itemTitle, itemLink }) => {
+  const NavMenuItem = ({ itemTitle, itemLink, ...props }) => {
     return (
-      <li className={classes.menu_item} onClick={() => setMenuOpen(false)}>
+      <li className={props.class} onClick={() => setMenuOpen(false)}>
         <NavLink
           to={itemLink}
           className={({ isActive }) =>
@@ -16,6 +18,7 @@ const NavigationMenu = (props) => {
         >
           {itemTitle}
         </NavLink>
+        {props.children}
       </li>
     );
   };
@@ -32,6 +35,33 @@ const NavigationMenu = (props) => {
         <NavMenuItem itemTitle="Kenya" itemLink="/kenya-safaris" />
         <NavMenuItem itemTitle="Tanzania" itemLink="/tanzania-safaris" />
         <NavMenuItem itemTitle="Rwanda" itemLink="/rwanda-safaris" />
+        <NavMenuItem
+          class={styles.dav__dropdown_wrapper}
+          itemLink="/more-safaris"
+          itemTitle="More Safaris"
+        >
+          <ul className={styles.dav_dropdown}>
+            <NavMenuItem itemTitle="Botswana" itemLink="/botswana-safaris" />
+            <NavMenuItem
+              itemTitle="South Africa"
+              itemLink="/south-africa-safaris"
+            />
+            <NavMenuItem itemTitle="Zimbabwe" itemLink="/zimbabwe-safaris" />
+            <NavMenuItem itemTitle="Zambia" itemLink="/zambia-safaris" />
+            <NavMenuItem itemTitle="Namibia" itemLink="/namibia-safaris" />
+            <NavMenuItem itemTitle="Egypt" itemLink="/egypt-safaris" />
+
+            <NavMenuItem
+              itemTitle="Madagascar"
+              itemLink="/madagascar-safaris"
+            />
+            <NavMenuItem
+              itemTitle="Seychelles"
+              itemLink="/seychelles-safaris"
+            />
+          </ul>
+        </NavMenuItem>
+
         <NavMenuItem itemTitle="About Us" itemLink="/about-us" />
         <NavMenuItem itemTitle="Safari Updates" itemLink="/safari-updates" />
         <NavMenuItem itemTitle="Community" itemLink="/community-support" />
