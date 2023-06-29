6b4ad2c012a802da16404615249f1705de97bacb
Moxhus <kimeramoses001@gmail.com>
2023-06-29 20:30:47 +0300

    feat: added API KEY to all requests

--- src/Adapter/index.js
+++ src/Adapter/index.js
@@ -17,11 +17,10 @@ export const Requestor = async (config) => {
   config.headers = {
     "Content-type": "application/json",
     "X-API-KEY": "d18a36f4-aa97-4b5c-ade0-4aacc0d70a5b",
+    Authorization: token ? `Bearer ${token}` : "",
     ...config.headers,
   };
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
+
   try {
     const { url, data } = config;
     const res = await axios(`${baseUrl}${url}`, {
