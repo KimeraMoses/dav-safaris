e74b187d8d748bd02deefc75bceef6a420c914b9
ssemugenyiisaac2@gmail.com <ssemugenyiisaac2@gmail.com>
2023-07-04 18:40:42 +0300

    (fix): Dropdown disappear on click on one of the countries

--- src/components/Navigation/NavigationMenu.js
+++ src/components/Navigation/NavigationMenu.js
@@ -4,9 +4,10 @@ import { Slide } from "react-slideshow-image";
 import "react-slideshow-image/dist/styles.css";
 import classes from "./NavigationMenu.module.css";
 import { Col, Row } from "react-bootstrap";
-import { fetchAllCountrys } from "../../store/Actions/CountryActions";
-import { useSelector, useDispatch } from "react-redux";
-import { selectAllCountries } from "../../store/Slices/countrySlice";
+import { ArrowDropDown } from "@material-ui/icons";
+// import { fetchAllCountrys } from "../../store/Actions/CountryActions";
+// import { useSelector, useDispatch } from "react-redux";
+// import { selectAllCountries } from "../../store/Slices/countrySlice";
 import { useAllCountries } from "../../hooks";
 
 const NavigationMenu = (props) => {
@@ -15,30 +16,37 @@ const NavigationMenu = (props) => {
   // const fetchedCountries = useSelector(selectAllCountries);
   // const dispatch = useDispatch();
   // const [allCountries, setAllCountries] = useState([]);
+  const [dropDownOpen, setDropDownOpen] = useState(true);
   const [isHovered, setIsHovered] = useState(false);
   const [hoveredCountry, setHoveredCountry] = useState({});
-  // useEffect(() => {
-  //   dispatch(fetchAllCountrys());
-  //   setAllCountries(fetchedCountries?.countries);
+  useEffect(() => {
+    setDropDownOpen(true);
 
-  //   // eslint-disable-next-line react-hooks/exhaustive-deps
-  // }, []);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [dropDownOpen]);
 
   const NavMenuItem = ({ itemTitle, itemLink, ...props }) => {
     return (
-      <li className={props.class} onClick={() => setMenuOpen(false)}>
+      <li
+        className={props.class}
+        onClick={() => setMenuOpen(false)}
+        // onMouseEnter={props.MouseEnter}
+        // onMouseLeave={props.MouseLeave}
+      >
         <NavLink
           to={itemLink}
           className={({ isActive }) =>
             classes.nav_link + (isActive ? ` ${classes.active}` : "")
           }
         >
-          {itemTitle}
+          {itemTitle} {itemTitle === "Other Destinations" && <ArrowDropDown />}
         </NavLink>
         {props.children}
       </li>
     );
   };
+
+  const closeDropDown = () => setDropDownOpen(false);
 
   const hoverHandler = (e) => {
     setIsHovered(true);
@@ -47,7 +55,7 @@ const NavigationMenu = (props) => {
     );
   };
   const mouseLeaveHandler = () => setIsHovered(false);
-  console.log(hoveredCountry);
+  // console.log(hoveredCountry);
 
   return (
     <div
@@ -66,79 +74,87 @@ const NavigationMenu = (props) => {
           itemLink="/more-safaris"
           itemTitle="Other Destinations"
         >
-          <div className={classes.dav__dropdown_wrapper}>
-            <Row className={classes.dav_dropdown}>
-              <Col lg={8} md={6} sm={4} xs={12}>
-                <Row>
-                  {countries?.slice(4).map((country) => (
-                    <Col lg={3} md={6} sm={6} xs={6} key={country.id}>
-                      <NavMenuItem
-                        key={country.id}
-                        itemTitle={country.name}
-                        itemLink={country.slug}
-                        // onMouseEnter={hoverHandler}
-                        // onMouseLeave={mouseLeaveHandler}
-                      />
-                    </Col>
-                  ))}
-                </Row>
-              </Col>
-
-              <Col
-                className={classes.dropdown_slider}
-                lg={4}
-                md={6}
-                sm={3}
-                xs={0}
-              >
-                {countries?.length > 0 && isHovered ? (
-                  <Link to={hoveredCountry.slug} key={hoveredCountry.id}>
-                    <div
-                      className={classes.dropdown_slide}
-                      style={{
-                        backgroundImage: `url(${hoveredCountry.countryImage})`,
-                        backgroundPosition: "center center",
-                        backgroundSize: "cover",
-                      }}
-                    >
-                      <span>
-                        <h2>{hoveredCountry.name}</h2>
-                      </span>
-                      <div className={classes.dropdown_slide__paragraph}>
-                        <p>{`${hoveredCountry.description.substring(
-                          4,
-                          65
-                        )}...`}</p>
-                      </div>
-                    </div>
-                  </Link>
-                ) : (
-                  <Slide easing="ease" arrows={false} duration={2000}>
+          {dropDownOpen && (
+            <div className={classes.dav__dropdown_wrapper}>
+              <Row className={classes.dav_dropdown}>
+                <Col lg={8} md={6} sm={4} xs={12}>
+                  <Row>
                     {countries?.slice(4).map((country) => (
-                      <Link to={country.slug} key={country.id}>
-                        <div
-                          className={classes.dropdown_slide}
-                          style={{
-                            backgroundImage: `url(${country.countryImage})`,
-                            backgroundPosition: "center center",
-                            backgroundSize: "cover",
-                          }}
-                        >
-                          <span>
-                            <h2>{country.name}</h2>
-                          </span>
-                          <div className={classes.dropdown_slide__paragraph}>
-                            <p>{`${country.description.substring(
-                              4,
-                              65
-                            )}...`}</p>
-                          </div>
-                        </div>
-                      </Link>
+                      <Col
+                        lg={3}
+                        md={6}
+                        sm={6}
+                        xs={6}
+                        key={country.id}
+                        onClick={closeDropDown}
+                      >
+                        <NavMenuItem
+                          key={country.id}
+                          itemTitle={country.name}
+                          itemLink={country.slug}
+                          MouseEnter={hoverHandler}
+                          MouseLeave={mouseLeaveHandler}
+                        />
+                      </Col>
                     ))}
-                  </Slide>
-                )}
-                {/* <Slide easing="ease" arrows={false} duration={2000}>
+                  </Row>
+                </Col>
+
+                <Col
+                  className={classes.dropdown_slider}
+                  lg={4}
+                  md={6}
+                  sm={3}
+                  xs={0}
+                >
+                  {countries?.length > 0 && isHovered ? (
+                    <Link to={hoveredCountry.slug} key={hoveredCountry.id}>
+                      <div
+                        className={classes.dropdown_slide}
+                        style={{
+                          backgroundImage: `url(${hoveredCountry.countryImage})`,
+                          backgroundPosition: "center center",
+                          backgroundSize: "cover",
+                        }}
+                      >
+                        <span>
+                          <h2>{hoveredCountry.name}</h2>
+                        </span>
+                        <div className={classes.dropdown_slide__paragraph}>
+                          <p>{`${hoveredCountry.description.substring(
+                            4,
+                            65
+                          )}...`}</p>
+                        </div>
+                      </div>
+                    </Link>
+                  ) : (
+                    <Slide easing="ease" arrows={false} duration={2000}>
+                      {countries?.slice(4).map((country) => (
+                        <Link to={country.slug} key={country.id}>
+                          <div
+                            className={classes.dropdown_slide}
+                            style={{
+                              backgroundImage: `url(${country.countryImage})`,
+                              backgroundPosition: "center center",
+                              backgroundSize: "cover",
+                            }}
+                          >
+                            <span>
+                              <h2>{country.name}</h2>
+                            </span>
+                            <div className={classes.dropdown_slide__paragraph}>
+                              <p>{`${country.description.substring(
+                                4,
+                                65
+                              )}...`}</p>
+                            </div>
+                          </div>
+                        </Link>
+                      ))}
+                    </Slide>
+                  )}
+                  {/* <Slide easing="ease" arrows={false} duration={2000}>
                   {allCountries?.map((country) => (
                     <Link to={country.slug} key={country.id}>
                       <div
@@ -159,9 +175,10 @@ const NavigationMenu = (props) => {
                     </Link>
                   ))}
                 </Slide> */}
-              </Col>
-            </Row>
-          </div>
+                </Col>
+              </Row>
+            </div>
+          )}
         </NavMenuItem>
 
         <NavMenuItem itemTitle="About Us" itemLink="/about-us" />
