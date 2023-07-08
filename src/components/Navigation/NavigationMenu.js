8e87af5fe3970c8aa552e1a522f6a50d182f6426
ssemugenyiisaac2@gmail.com <ssemugenyiisaac2@gmail.com>
2023-07-08 12:33:34 +0300

    (fix): fixed the responsivenes of the navigation and its components on all devices

--- src/components/Navigation/NavigationMenu.js
+++ src/components/Navigation/NavigationMenu.js
@@ -1,61 +1,53 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { Link, NavLink } from "react-router-dom";
 import { Slide } from "react-slideshow-image";
 import "react-slideshow-image/dist/styles.css";
 import classes from "./NavigationMenu.module.css";
 import { Col, Row } from "react-bootstrap";
-import { ArrowDropDown } from "@material-ui/icons";
-// import { fetchAllCountrys } from "../../store/Actions/CountryActions";
-// import { useSelector, useDispatch } from "react-redux";
-// import { selectAllCountries } from "../../store/Slices/countrySlice";
+
 import { useAllCountries } from "../../hooks";
+import { ArrowDropDown } from "@material-ui/icons";
 
 const NavigationMenu = (props) => {
   const { countries } = useAllCountries();
   const { menuOpen, setMenuOpen } = props;
-  // const fetchedCountries = useSelector(selectAllCountries);
-  // const dispatch = useDispatch();
-  // const [allCountries, setAllCountries] = useState([]);
   const [dropDownOpen, setDropDownOpen] = useState(true);
-  const [isHovered, setIsHovered] = useState(false);
-  const [hoveredCountry, setHoveredCountry] = useState({});
+  const slideShowRef = useRef();
+
   useEffect(() => {
     setDropDownOpen(true);
 
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [dropDownOpen]);
 
-  const NavMenuItem = ({ itemTitle, itemLink, ...props }) => {
+  const NavMenuItem = ({ itemTitle, itemLink, id, ...props }) => {
     return (
       <li
         className={props.class}
-        onClick={() => setMenuOpen(false)}
-        // onMouseEnter={props.MouseEnter}
-        // onMouseLeave={props.MouseLeave}
+        onClick={() => {
+          setMenuOpen(false);
+        }}
       >
         <NavLink
           to={itemLink}
           className={({ isActive }) =>
             classes.nav_link + (isActive ? ` ${classes.active}` : "")
           }
+          id={id}
+          onMouseEnter={(event) => {
+            slideShowRef.current.goTo(parseInt(event.target?.id));
+          }}
         >
-          {itemTitle} {itemTitle === "Other Destinations" && <ArrowDropDown />}
+          {itemTitle}
         </NavLink>
         {props.children}
       </li>
     );
   };
 
-  const closeDropDown = () => setDropDownOpen(false);
-
-  const hoverHandler = (e) => {
-    setIsHovered(true);
-    setHoveredCountry(
-      countries?.filter((country) => country.name === e.target.innerText)[0]
-    );
+  const closeDropDown = () => {
+    setDropDownOpen(false);
   };
-  const mouseLeaveHandler = () => setIsHovered(false);
-  // console.log(hoveredCountry);
 
   return (
     <div
@@ -69,31 +61,30 @@ const NavigationMenu = (props) => {
         <NavMenuItem itemTitle="Kenya" itemLink="/kenya-safaris" />
         <NavMenuItem itemTitle="Tanzania" itemLink="/tanzania-safaris" />
         <NavMenuItem itemTitle="Rwanda" itemLink="/rwanda-safaris" />
-        <NavMenuItem
-          class={classes.more}
-          itemLink="/more-safaris"
-          itemTitle="Other Destinations"
-        >
+        <li className={classes.more}>
+          <span className={classes.nav_link}>
+            Other Destinations <ArrowDropDown />
+          </span>
           {dropDownOpen && (
             <div className={classes.dav__dropdown_wrapper}>
               <Row className={classes.dav_dropdown}>
-                <Col lg={8} md={6} sm={4} xs={12}>
+                <Col lg={8} md={6} sm={7} xs={12}>
                   <Row>
-                    {countries?.slice(4).map((country) => (
+                    {countries?.slice(4).map((country, index) => (
                       <Col
                         lg={3}
-                        md={6}
-                        sm={6}
+                        md={4}
+                        sm={4}
                         xs={6}
-                        key={country.id}
+                        key={index}
+                        id={index}
                         onClick={closeDropDown}
                       >
                         <NavMenuItem
-                          key={country.id}
+                          key={index}
+                          id={index}
                           itemTitle={country.name}
                           itemLink={country.slug}
-                          MouseEnter={hoverHandler}
-                          MouseLeave={mouseLeaveHandler}
                         />
                       </Col>
                     ))}
@@ -104,36 +95,21 @@ const NavigationMenu = (props) => {
                   className={classes.dropdown_slider}
                   lg={4}
                   md={6}
-                  sm={3}
+                  sm={5}
                   xs={0}
                 >
-                  {countries?.length > 0 && isHovered ? (
-                    <Link to={hoveredCountry.slug} key={hoveredCountry.id}>
-                      <div
-                        className={classes.dropdown_slide}
-                        style={{
-                          backgroundImage: `url(${hoveredCountry.countryImage})`,
-                          backgroundPosition: "center center",
-                          backgroundSize: "cover",
-                        }}
-                      >
-                        <span>
-                          <h2>{hoveredCountry.name}</h2>
-                        </span>
-                        <div className={classes.dropdown_slide__paragraph}>
-                          <p>{`${hoveredCountry.description.substring(
-                            4,
-                            65
-                          )}...`}</p>
-                        </div>
-                      </div>
-                    </Link>
-                  ) : (
-                    <Slide easing="ease" arrows={false} duration={2000}>
-                      {countries?.slice(4).map((country) => (
+                  {countries?.length > 0 && (
+                    <Slide
+                      easing="ease"
+                      arrows={false}
+                      duration={2000}
+                      ref={slideShowRef}
+                      autoplay={true}
+                    >
+                      {countries?.slice(4).map((country, index) => (
                         <Link
                           to={country.slug}
-                          key={country.id}
+                          key={index}
                           onClick={closeDropDown}
                         >
                           <div
@@ -148,42 +124,18 @@ const NavigationMenu = (props) => {
                               <h2>{country.name}</h2>
                             </span>
                             <div className={classes.dropdown_slide__paragraph}>
-                              <p>{`${country.description.substring(
-                                4,
-                                65
-                              )}...`}</p>
+                              <p>{`${country.summary.substring(0, 65)}...`}</p>
                             </div>
                           </div>
                         </Link>
                       ))}
                     </Slide>
                   )}
-                  {/* <Slide easing="ease" arrows={false} duration={2000}>
-                  {allCountries?.map((country) => (
-                    <Link to={country.slug} key={country.id}>
-                      <div
-                        className={classes.dropdown_slide}
-                        style={{
-                          backgroundImage: `url(${country.countryImage})`,
-                          backgroundPosition: "center center",
-                          backgroundSize: "cover",
-                        }}
-                      >
-                        <span>
-                          <h2>{country.name}</h2>
-                        </span>
-                        <div className={classes.dropdown_slide__paragraph}>
-                          <p>{`${country.description.substring(4, 65)}...`}</p>
-                        </div>
-                      </div>
-                    </Link>
-                  ))}
-                </Slide> */}
                 </Col>
               </Row>
             </div>
           )}
-        </NavMenuItem>
+        </li>
 
         <NavMenuItem itemTitle="About Us" itemLink="/about-us" />
         <NavMenuItem itemTitle="Safari Updates" itemLink="/safari-updates" />
