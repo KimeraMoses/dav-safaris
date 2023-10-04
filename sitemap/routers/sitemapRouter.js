6e86f0db171826dd5bc9b32b9f72871fcd7e7e7d
Moxhus <kimeramoses001@gmail.com>
2023-10-04 17:23:26 +0300

    feat: removed unneccessary console logs

--- sitemap/routers/sitemapRouter.js
+++ sitemap/routers/sitemapRouter.js
@@ -1,83 +1,83 @@
-const express = require("express");
-const { SitemapStream, streamToPromise } = require("sitemap");
-const Tour = require("../modals/tourModal");
-const Post = require("../modals/postModal");
-const LanguagePost = require("../modals/languagePostModal");
+const express = require('express');
+const { SitemapStream, streamToPromise } = require('sitemap');
+const Tour = require('../modals/tourModal');
+const Post = require('../modals/postModal');
+const LanguagePost = require('../modals/languagePostModal');
 const date = new Date().toISOString();
-const zlib = require("zlib");
+const zlib = require('zlib');
 const router = express.Router();
 
 const staticRoutes = [
   {
-    path: "/uganda-safaris",
+    path: '/uganda-safaris',
   },
   {
-    path: "/kenya-safaris",
+    path: '/kenya-safaris',
   },
   {
-    path: "/tanzania-safaris",
+    path: '/tanzania-safaris',
   },
   {
-    path: "/rwanda-safaris",
+    path: '/rwanda-safaris',
   },
   {
-    path: "/uganda-safaris/gorilla-wildlife-safaris",
+    path: '/uganda-safaris/gorilla-wildlife-safaris',
   },
   {
-    path: "/uganda-safaris/uganda-birding-safaris",
+    path: '/uganda-safaris/uganda-birding-safaris',
   },
   {
-    path: "/uganda-safaris/uganda-cultural-tours",
+    path: '/uganda-safaris/uganda-cultural-tours',
   },
   {
-    path: "/uganda-safaris/mountaineering",
+    path: '/uganda-safaris/mountaineering',
   },
   {
-    path: "/kenya-safaris/kenya-wildlife-safaris",
+    path: '/kenya-safaris/kenya-wildlife-safaris',
   },
   {
-    path: "/kenya-safaris/kenya-birding-safaris",
+    path: '/kenya-safaris/kenya-birding-safaris',
   },
   {
-    path: "/tanzania-safaris/tanzania-wildlife-safaris",
+    path: '/tanzania-safaris/tanzania-wildlife-safaris',
   },
   {
-    path: "/tanzania-safaris/mountain-hiking-safaris",
+    path: '/tanzania-safaris/mountain-hiking-safaris',
   },
   {
-    path: "/tanzania-safaris/tanzania-birding-tours",
+    path: '/tanzania-safaris/tanzania-birding-tours',
   },
   {
-    path: "/rwanda-safaris/rwanda-gorilla-wildlife-safaris",
+    path: '/rwanda-safaris/rwanda-gorilla-wildlife-safaris',
   },
   {
-    path: "/rwanda-safaris/rwanda-culture-safaris",
+    path: '/rwanda-safaris/rwanda-culture-safaris',
   },
   {
-    path: "/rwanda-safaris/rwanda-birding-safaris",
+    path: '/rwanda-safaris/rwanda-birding-safaris',
   },
   {
-    path: "/about-us",
+    path: '/about-us',
   },
   {
-    path: "/safari-updates",
+    path: '/safari-updates',
   },
   {
-    path: "/community-support",
+    path: '/community-support',
   },
   {
-    path: "/contact-us",
+    path: '/contact-us',
   },
   {
-    path: "/safari-updates/languages",
+    path: '/safari-updates/languages',
   },
 ];
 
 let sitemap;
 
-router.get("/", async function (req, res) {
-  res.header("Content-Type", "application/xml");
-  res.header("Content-Encoding", "gzip");
+router.get('/', async function (req, res) {
+  res.header('Content-Type', 'application/xml');
+  res.header('Content-Encoding', 'gzip');
 
   // If we have a cached entry send it
   if (sitemap) return res.send(sitemap);
@@ -95,39 +95,36 @@ router.get("/", async function (req, res) {
     );
     // Base url of our site
     (smStream = new SitemapStream({
-      hostname: "https://davsafaris.com/",
+      hostname: 'https://davsafaris.com/',
     })),
       (pipeline = smStream.pipe(zlib.createGzip()));
 
-    // Write tours URL to the stream
-    console.log("total tours", tours.length); //lastTotal 146
+    // Write tours URL to the stream//lastTotal 146
     tours.forEach((item) =>
       smStream.write({
         url: item,
         lastmod: date,
-        changefreq: "daily",
+        changefreq: 'daily',
         priority: 0.7,
       })
     );
 
     // Write safari updates URL to the stream
-    console.log("total posts", posts.length); //lastRead 61
     posts.forEach((item) =>
       smStream.write({
         url: item,
         lastmod: date,
-        changefreq: "daily",
+        changefreq: 'daily',
         priority: 0.7,
       })
     );
 
     // Write safari updates URL to the stream
-    console.log("total language posts", languagePosts.length); //lastTotal 134
     languagePosts.forEach((item) =>
       smStream.write({
         url: item,
         lastmod: date,
-        changefreq: "daily",
+        changefreq: 'daily',
         priority: 0.7,
       })
     );
@@ -137,7 +134,7 @@ router.get("/", async function (req, res) {
       smStream.write({
         url: item.path,
         lastmod: date,
-        changefreq: "monthly",
+        changefreq: 'monthly',
         priority: 0.9,
       });
     });
@@ -147,7 +144,7 @@ router.get("/", async function (req, res) {
     smStream.end();
 
     // Stream write the response
-    pipeline.pipe(res).on("error", (e) => {
+    pipeline.pipe(res).on('error', (e) => {
       throw e;
     });
   } catch (err) {
--- src/components/HomePage/Home.js
+++ src/components/HomePage/Home.js
@@ -73,8 +73,6 @@ const Home = () => {
     });
   };
 
-  console.log('Tours', tours);
-
   return (
     <Wrapper>
       <SEO
